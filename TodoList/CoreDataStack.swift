5b4650c06ae374a6fb80de53b75da094242efbd4
Tony Saavedra <ajsaavedrax@gmail.com>
2016-07-26 21:28:35 -0700

    Initial repo with core data stack setup

---
+++ TodoList/CoreDataStack.swift
@@ -0,0 +1,42 @@
+import Foundation
+import CoreData
+
+class DataController: NSObject {
+    private lazy var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls[urls.endIndex.predecessor()]
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelURL = NSBundle.mainBundle().URLForResource("TodoList", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("TodoList.sqlite")
+
+        do {
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        } catch {
+            let userInfo: [String: AnyObject] = [
+                NSLocalizedDescriptionKey: "Failed to initialize the application's saved data",
+                NSLocalizedFailureReasonErrorKey: "There was an error creating or loading the application's saved data",
+                NSUnderlyingErrorKey: error as NSError
+            ]
+            
+            let wrappedError = NSError(domain: "com.tonyjs.CoreDataError", code: 9999, userInfo: userInfo)
+            print("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+
+        return coordinator
+    }()
+
+    public lazy var managedObjectContext: NSManagedObjectContext = {
+        let coordinator = self.persistentStoreCoordinator
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = coordinator
+        return managedObjectContext
+    }()
+}
