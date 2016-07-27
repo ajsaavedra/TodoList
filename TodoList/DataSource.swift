9169c61e3737a26c9da68d0d47881c48553192c5
Tony Saavedra <ajsaavedrax@gmail.com>
2016-07-27 00:46:15 -0700

    Refactored view controller to not get data

---
+++ TodoList/DataSource.swift
@@ -0,0 +1,50 @@
+import Foundation
+import UIKit
+import CoreData
+
+class DataSource: NSObject, UITableViewDataSource {
+
+    private let tableView: UITableView
+
+    let managedObjectContext = DataController.sharedInstance.managedObjectContext
+    
+    lazy var fetchedResultsController: TodoFetchedResultsController = {
+        let controller = TodoFetchedResultsController(managedObjectContext: self.managedObjectContext,
+                                                      withTableView: self.tableView)
+        return controller
+    }()
+    
+    init(tableView: UITableView) {
+        self.tableView = tableView
+    }
+    
+    func objectAtIndexPath(indexPath: NSIndexPath) -> NSManagedObject {
+        return fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return fetchedResultsController.sections?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let section = fetchedResultsController.sections?[section] else { return 0 }
+        return section.numberOfObjects
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        return configureCell(cell, atIndexPath: indexPath)
+    }
+    
+    private func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let item = fetchedResultsController.objectAtIndexPath(indexPath) as! Item
+        cell.textLabel?.text = item.text
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        let item = fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
+        managedObjectContext.deleteObject(item)
+        DataController.sharedInstance.saveContext()
+    }
+}
--- TodoList/TodoListController.swift
+++ TodoList/TodoListController.swift
@@ -3,42 +3,13 @@ import CoreData
 
 class TodoListController: UITableViewController, NSFetchedResultsControllerDelegate {
 
-    let managedObjectContext = DataController.sharedInstance.managedObjectContext
-
-    lazy var fetchedResultsController: TodoFetchedResultsController = {
-        let controller = TodoFetchedResultsController(managedObjectContext: self.managedObjectContext,
-                                                      withTableView: self.tableView)
-        return controller
+    lazy var dataSource: DataSource = {
+        return DataSource(tableView: self.tableView)
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return fetchedResultsController.sections?.count ?? 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        guard let section = fetchedResultsController.sections?[section] else { return 0 }
-        return section.numberOfObjects
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        return configureCell(cell, atIndexPath: indexPath)
-    }
-
-    private func configureCell(cell: UITableViewCell, atIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let item = fetchedResultsController.objectAtIndexPath(indexPath) as! Item
-        cell.textLabel?.text = item.text
-        return cell
-    }
-    
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        let item = fetchedResultsController.objectAtIndexPath(indexPath) as! NSManagedObject
-        managedObjectContext.deleteObject(item)
-        DataController.sharedInstance.saveContext()
+        tableView.dataSource = dataSource
     }
     
     override func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCellEditingStyle {
@@ -48,7 +19,7 @@ class TodoListController: UITableViewController, NSFetchedResultsControllerDeleg
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "ShowItem" {
             guard let destinationController = segue.destinationViewController as? DetailViewController, indexPath = tableView.indexPathForSelectedRow else { return }
-            let item = fetchedResultsController.objectAtIndexPath(indexPath) as! Item
+            let item = dataSource.objectAtIndexPath(indexPath) as! Item
             destinationController.item = item
         }
     }
